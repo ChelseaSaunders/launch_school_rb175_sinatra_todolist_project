babcc6c12568c586eb3425ddabf5dc7065d5494a
Chelsea Saunders <c_chilcoat@msn.com>
2021-09-28 14:13:24 -0700

    code through lesson 12

--- todo.rb
+++ todo.rb
@@ -2,6 +2,59 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do 
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+# GET   /lists        -> view all lists
+# GET   /lists/new    -> new list form
+# POST  /lists        -> create new list
+# GET   /lists/1      -> view a single list (number will be list id)
+# pattern is: resource_type/resource_id
+
+# View list of lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# Return error message if the name is invalid. Return nil if the name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover?(name.length)
+    "The list name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "The list name must be unique."
+  else
+    nil
+  end
+end
+
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+
+  if error = error_for_list_name(list_name)
+    session[:error] = error
+    erb :new_list, layout: :layout  
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
 end
