e4444247718a2f9ab04f2202333d91920f116c09
Chelsea Saunders <c_chilcoat@msn.com>
2021-10-01 13:35:23 -0700

    preparing for heroku deployment

--- todo.rb
+++ todo.rb
@@ -1,7 +1,8 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
+
 
 configure do 
   enable :sessions
